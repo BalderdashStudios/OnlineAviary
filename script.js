ff5a16846351904fefd6f718beb34bc73f49b9d4
BalderdashStudios <h.ray.emerald@gmail.com>
2024-02-21 13:43:58 -0800

    update images to card system + highlight on hover

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {	
 	$(".imgselect").mouseenter(function() { 
 		$(this).css("filter", "grayscale(0%)");
-		$(this).css("width", "200%");
+		$(this).parent().parent().parent().addClass("col-main");
 	});
 	
 	$(".imgselect").mouseleave(function() { 
 		$(this).css("filter", "grayscale(100%)");
+		$(this).parent().parent().parent().removeClass("col-main");
 	});
 	
 });
