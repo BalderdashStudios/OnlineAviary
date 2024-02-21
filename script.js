249a311306a6bce16a1a20195b046802d2bad4ce
BalderdashStudios <h.ray.emerald@gmail.com>
2024-02-21 13:49:59 -0800

    fix JS to only highlight with cards

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {	
 	$(".imgselect").mouseenter(function() { 
 		$(this).css("filter", "grayscale(0%)");
-		$(this).parent().parent().parent().addClass("col-main");
+		$(this).parent().parent().parent().filter(".card").addClass("col-main");
 	});
 	
 	$(".imgselect").mouseleave(function() { 
 		$(this).css("filter", "grayscale(100%)");
-		$(this).parent().parent().parent().removeClass("col-main");
+		$(this).parent().parent().parent().filter(".card").removeClass("col-main");
 	});
 	
 });
