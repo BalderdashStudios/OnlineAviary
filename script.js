e3594d6e66ad79f24e63d629a33b879232b3627b
BalderdashStudios <h.ray.emerald@gmail.com>
2024-02-28 13:57:24 -0800

    Updated Search Bar

--- script.js
+++ script.js
@@ -11,12 +11,18 @@ $(document).ready(function() {
 
 	$('#mySel').change(function(){
 		sel = $(this).val();
-		if (sel == 'photos'){
+		if (sel == 'Condor'){
 			window.location.href = 'birdPageTemplate.html';
-		} else if (sel == 'books'){
-			window.location.href = 'birdPageTemplate.html';
-		}else if (sel == 'recipes'){
-			window.location.href = 'budgie.html';
+		} else if (sel == 'Magpie'){
+			window.location.href = 'magpie.html';
+		}else if (sel == 'Horned Owl'){
+			window.location.href = 'hornedOwl.html';
+		}else if (sel == 'Curassow'){
+			window.location.href = 'curassow.html';
+		}else if (sel == 'Harpy Eagle'){
+			window.location.href = 'harpyEagle.html';
+		}else if (sel == 'Bananaquit'){
+			window.location.href = 'bananaquit.html';
 		}
 	});
 })
